8576ea16f6340277abc2b8efb4ca7f76bef3b69d
BG4JTS <zhaodachuan2@outlook.com>
2026-09-05 14:04:45 +0800

    配置信息

--- src/config.ts
+++ src/config.ts
@@ -9,7 +9,7 @@ import { LinkPreset } from "./types/config";
 
 export const siteConfig: SiteConfig = {
 	title: "BG4JTS' blog",
-	subtitle: "BG4JTS 的博客~",
+	subtitle: "BG4JTS 的博客 ~",
 	lang: "zh_CN", // Language code, e.g. 'en', 'zh_CN', 'ja', etc.
 	themeColor: {
 		hue: 215, // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
@@ -53,9 +53,9 @@ export const navBarConfig: NavBarConfig = {
 };
 
 export const profileConfig: ProfileConfig = {
-	avatar: "assets/images/demo-avatar.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
-	name: "Lorem Ipsum",
-	bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+	avatar: "assets/images/avatar.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+	name: "BG4JTS",
+	bio: "（写了一些很厉害的简介）",
 	links: [
 		{
 			name: "GitHub",
